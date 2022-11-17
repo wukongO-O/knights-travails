d8bacee8cbc4a664202bd38e33ccbb531d44e2d0
wukongO-O <l.shenglan@gmail.com>
2022-11-17 17:09:05 -0500

    create 8x8 chess board

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('test');
+import './style.css';
+
+const gameArea = document.createElement('div');
+document.body.appendChild(gameArea);
+//create an 8 x 8, black-n-white chess board via table element
+const gameBoard = document.createElement('table');
+for (let i = 0; i < 8; i++) {
+    let row = document.createElement('tr');
+    for (let j = 0; j < 8; j++) {
+        const cell = document.createElement('td');
+        row.appendChild(cell);
+        if ((i + j) % 2 == 0) {
+            cell.setAttribute('class', 'cell whiteCell');
+        } else {
+            cell.setAttribute('class', 'cell blackCell');
+        }
+    };
+    gameBoard.appendChild(row);
+};
+gameArea.appendChild(gameBoard);
+
+gameBoard.setAttribute('width', '400px');
+gameBoard.setAttribute('height', '400px');
+gameBoard.setAttribute('cellspacing', '0');
