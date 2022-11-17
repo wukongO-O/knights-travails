8f044eaea5579e0339dc7f1ea507af95ab8781f6
wukongO-O <l.shenglan@gmail.com>
2022-11-17 12:05:04 -0500

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Knights Travails',
+            myPageHeader: '♟ Knights Travails',
+            template: './src/index.html'
+        }),
+      ],
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
