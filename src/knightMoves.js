200d577a62e019460d9ff96dbfa65b35f56c9794
wukongO-O <l.shenglan@gmail.com>
2022-11-29 15:49:31 -0500

    add knighMoves function

--- src/knightMoves.js
+++ src/knightMoves.js
@@ -2,87 +2,28 @@ const knightMoves = (a, b) => {
     const chessBoardGrid = buildGrid(8, 8);
     const movesAdjList = buildMovesGraph(chessBoardGrid);
 
-    return bfs(movesAdjList, String(a), String(b));
+    return shortestPath(movesAdjList, String(a), String(b));
 };
 
-function bfs(graph, start, end) {
-    let queue = [[start, []]],
-        seen = new Set;
+const shortestPath = (graph, a, b) => {
+    const visitedNodes = new Set();
+    let queue = [[a, 0, []]];
 
-    while (queue.length) {
-        let [curVert, [...path]] = queue.shift();
-        path.push(curVert);
-        if (curVert === end) return path;
-
-        if (!seen.has(curVert) && graph[curVert]) {
-            queue.push(...graph[curVert].map(v => [v, path]));
-        }
-        seen.add(curVert);
-    }
-
-    return -1;
-}
-
-// const shortestPath = (graph, nodeA, nodeB) => {
-//     const visitedNodes = new Set();
-//     const a = String(nodeA);
-//     const b = String(nodeB);
-//     let queue = [[a, 0, []]];
-
-//     while (queue.length > 0) {
-//         let [currentNode, moves, [...path]] = queue.shift();
+    while (queue.length > 0) {
+        let [currentNode, moves, [...path]] = queue.shift();
+        path.push(currentNode);
         
-//         if (String(currentNode) === String(b)) return {moves, path};
+        if (String(currentNode) === String(b)) return {moves, path};
 
-//         if (! visitedNodes.has(currentNode) && graph
-//         [currentNode]) {
-//             queue.push(...graph[currentNode].map(n => [n, moves, path]));
-//         }
-//         visitedNodes.add(currentNode);
-//     }
-// };
+        if (! visitedNodes.has(currentNode)) {
+            const neighbors = graph[currentNode].map(neighbor => [neighbor, moves + 1, path]);
+            queue.push(...neighbors);
+            // queue.push(...graph[currentNode].map(neighbor => [neighbor, moves+1, path])); //condense above 2 lines to 1
+            visitedNodes.add(currentNode);
+        }
+    }
+};
 
-// const shortestPathI = (graph, a, b) => {
-//     const visitedNodes = new Set([a]);
-//     //const visitedNeighbors = new Set();
-//     let previous = new Map();
-//     let queue = [[a, 0]];
-//     let path = [a];
-
-//     while (queue.length > 0) {
-//         var [currentNode, moves] = queue.shift();
-
-//         if (String(currentNode) === String(b)) {
-//             return { moves, previous };
-//         }  
-       
-//         for (let neighbor of graph[currentNode]) {
-//             // if (hasPath(graph, neighbor, b, visitedNeighbors) == true) {
-//             //     path.push(neighbor);
-//             // }
-            
-//             if (! visitedNodes.has(neighbor)) {
-//                 previous.set(neighbor, currentNode);
-//                 visitedNodes.add(neighbor);
-//                 queue.push([neighbor, moves + 1]);
-//             }           
-//         };
-//     }
-
-//     return -1; //if no path is found
-// }
-// const hasPath = (graph, a, b, visited) => {
-//     if (a === b) return true;
-
-//     if (visited.has(a)) return false;
-//     visited.add(a);
-
-//     for (let neighborN of graph[a]) {
-//         if (hasPath(graph, neighborN, b, visited) === true) return true;
-//     };
-
-//     return false;
-// };
 //Build a grid w each node's value as a  chessboard cell's x-y coordinates
 const buildGrid = (row, col) => {
     let grid = [];
